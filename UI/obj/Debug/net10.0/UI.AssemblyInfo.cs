7202a09bde07b5100c2c0335a0e4d765dc0281e5
migue <miguel_rb25@hotmail.com>
2026-03-08 23:32:01 -0700

    Moved files

--- UI/obj/Debug/net10.0/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net10.0/UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fab5de2973fa66c8eae5cf3b9753815c00bcda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c136e6fb2fd7b239fd0189265e83d5ad52dd40a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
